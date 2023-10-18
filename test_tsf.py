1f36b346e4624fb7c02660c9fc5286b8ff4527bf
kunalchhabra2001 <62746736+kunalchhabra2001@users.noreply.github.com>
2023-10-18 12:38:42 +0530

    Update test_tsf.py

--- test_tsf.py
+++ test_tsf.py
@@ -28,48 +28,51 @@ def test(model, dataset, history_len, pred_len, batch_size):
         for idx in range(history_len, len(dataset.test_df)-pred_len, batch_size):
                 print("Yes test function is running")
                 past_ts_list = []
-                series_names_tokenized_iid_list = []
-                series_names_tokenized_am_list = []
+                # series_names_tokenized_iid_list = []
+                # series_names_tokenized_am_list = []
                 date_features_vector_list = []
                 future_ts_list = []
                 future_ts_list2 = []
                 related_ts_list = []
-                related_series_names_tokenized_iid_list = []
-                related_series_names_tokenized_am_list = []
+                # related_series_names_tokenized_iid_list = []
+                # related_series_names_tokenized_am_list = []
                 col = 'Price'
-                past_ts, series_names_tokenized, date_features_vector, future_date_features_vector, future_ts, related_ts, related_series_names_tokenized = dataset.get_io_i('test', col, idx, history_len, pred_len)
-                n = len(related_ts[0])
-                a = [[] for x in range(n)]
-                b = [[] for x in range(n)]
+                past_ts,date_features_vector, future_date_features_vector, future_ts, related_ts= dataset.get_io_i('test', col, idx, history_len, pred_len)
+                #past_ts, series_names_tokenized, date_features_vector, future_date_features_vector, future_ts, related_ts, related_series_names_tokenized = dataset.get_io_i('test', col, idx, history_len, pred_len)
+                # n = len(related_ts[0])
+                # a = [[] for x in range(n)]
+                # b = [[] for x in range(n)]
                 for i in range(batch_size):
                     if idx+i>=len(dataset.test_df)-pred_len:
                         break
                     col = 'Price'
-                    past_ts, series_names_tokenized, date_features_vector, future_date_features_vector, future_ts, related_ts, related_series_names_tokenized = dataset.get_io_i('test', col, idx+i, history_len, pred_len)
+                    past_ts,date_features_vector, future_date_features_vector, future_ts, related_ts= dataset.get_io_i('test', col, idx+i, history_len, pred_len)
+                    #past_ts, series_names_tokenized, date_features_vector, future_date_features_vector, future_ts, related_ts, related_series_names_tokenized = dataset.get_io_i('test', col, idx+i, history_len, pred_len)
                     past_ts_list.append(past_ts)
-                    series_names_tokenized_iid_list.append(series_names_tokenized['input_ids'])
-                    series_names_tokenized_am_list.append(series_names_tokenized['attention_mask'])
+                    # series_names_tokenized_iid_list.append(series_names_tokenized['input_ids'])
+                    # series_names_tokenized_am_list.append(series_names_tokenized['attention_mask'])
                     date_features_vector_list.append(date_features_vector)
                     future_ts_list.append(future_ts)
                     future_ts_list2.append(future_date_features_vector)
                     related_ts_list.append(related_ts)
-                    n = len(related_ts[0])
-                    for j in range(n):
-                        a[j].append(related_series_names_tokenized[j]['input_ids'])
-                        b[j].append(related_series_names_tokenized[j]['attention_mask'])
-                #print("Length of tokenized" , len(series_names_tokenized_iid_list))
-                series_names_tokenized = {'input_ids':torch.cat(tensors=series_names_tokenized_iid_list, dim=0), 'attention_mask':torch.cat(tensors=series_names_tokenized_am_list, dim=0)}
-                rel_series_final = []
-                for i in range(n):
-                    related_series_names_tokenized = {'input_ids':torch.cat(tensors=a[i], dim=0), 'attention_mask':torch.cat(tensors=b[i], dim=0)}
-                    rel_series_final.append(related_series_names_tokenized)
+                #     n = len(related_ts[0])
+                #     for j in range(n):
+                #         a[j].append(related_series_names_tokenized[j]['input_ids'])
+                #         b[j].append(related_series_names_tokenized[j]['attention_mask'])
+                # #print("Length of tokenized" , len(series_names_tokenized_iid_list))
+                # series_names_tokenized = {'input_ids':torch.cat(tensors=series_names_tokenized_iid_list, dim=0), 'attention_mask':torch.cat(tensors=series_names_tokenized_am_list, dim=0)}
+                # rel_series_final = []
+                # for i in range(n):
+                #     related_series_names_tokenized = {'input_ids':torch.cat(tensors=a[i], dim=0), 'attention_mask':torch.cat(tensors=b[i], dim=0)}
+                #     rel_series_final.append(related_series_names_tokenized)
                 #related_series_names_tokenized = {'input_ids':torch.cat(tensors=related_series_names_tokenized_iid_list, dim=0), 'attention_mask':torch.cat(tensors=related_series_names_tokenized_am_list, dim=0)}
                 past_ts = torch.cat(tensors=past_ts_list, dim=0)
                 date_features_vector = torch.cat(tensors=date_features_vector_list, dim=0)
                 related_ts = torch.cat(tensors=related_ts_list, dim=0)
                 future_ts = torch.cat(tensors=future_ts_list, dim=0)
                 future_ts2 = torch.cat(tensors=future_ts_list2, dim=0)
-                preds = model(past_ts, series_names_tokenized, date_features_vector, future_ts2, related_ts, rel_series_final, None, col)
+                preds = model(past_ts, None, date_features_vector, future_ts2, related_ts, None, None, col)
+                #preds = model(past_ts, series_names_tokenized, date_features_vector, future_ts2, related_ts, rel_series_final, None, col)
                 print(preds)
                 #print("Type of preds",preds.type)
                 for i in range(len(preds)):
@@ -89,14 +92,14 @@ parser = argparse.ArgumentParser(prog = 'TSF_Tester', description = 'Testing TiD
 parser.add_argument('--seed', type=int, default=42)
 parser.add_argument('--epochs', type=int, default=10)
 parser.add_argument('--batch_size', type=int, default=8)
-parser.add_argument('--lr', type=float, default=2e-5)
+parser.add_argument('--lr', type=float, default=2e-4)
 
 #parser.add_argument('--related_series_type', type=str, default='llm') # can be llm or random
 parser.add_argument('--global_model', type=bool, default=True)
 parser.add_argument('--global_encoder', type=bool, default=True)
 
-parser.add_argument('--history_len', type=int, default=4)
-parser.add_argument('--pred_len', type=int, default=2)
+parser.add_argument('--history_len', type=int, default=32)
+parser.add_argument('--pred_len', type=int, default=1)
 parser.add_argument('--text_dim', type=int, default=4)
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -116,10 +119,11 @@ if __name__ == '__main__':
     text_dim = args.text_dim
     batch_size = args.batch_size
     dataset = BankNiftyFuturesDataset(device)
-    model_config = {'input_dim':text_dim+history_len*(1+len(dataset.time_features_names)), 'hidden_dims':[4, 4], 'decoder_output_dim':pred_len, 'final_decoder_hidden':1, 'text_dim':text_dim}
+    model_config = {'input_dim':history_len*(1+len(dataset.time_features_names)), 'hidden_dims':[4, 4], 'decoder_output_dim':pred_len, 'final_decoder_hidden':2}
+    #model_config = {'input_dim':text_dim+history_len*(1+len(dataset.time_features_names)), 'hidden_dims':[4, 4], 'decoder_output_dim':pred_len, 'final_decoder_hidden':1, 'text_dim':text_dim}
 
     model = TideModel(model_config, pred_len, history_len, device, False, 0.0, global_model, dataset.all_series_names, global_encoder,len(dataset.all_series_names)).to(device)
-    path = '/mnt/nas/kunalchhabra/TIDE_Models/tsf_global_encoder__epoch_10'
+    path = '/mnt/nas/kunalchhabra/TIDE_Models/tsf_global_encoder_bn__epoch_10'
     model.load_state_dict(torch.load(path))
     
     loss_function = torch.nn.MSELoss()
